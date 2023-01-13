92580a867e1c599c5834ad93775caf1ff6e5ae77
uturkarslan <ugurturkarslan@gmail.com>
2023-01-13 20:12:25 +0300

    Implement parallel execution

--- pkg/traces/trace.go
+++ pkg/traces/trace.go
@@ -3,6 +3,7 @@ package traces
 import (
 	"os"
 	"strconv"
+	"sync"
 
 	"github.com/utr1903/newrelic-tracker-ingest/pkg/traces/apps"
 	"github.com/utr1903/newrelic-tracker-ingest/pkg/traces/data"
@@ -10,12 +11,33 @@ import (
 
 func Run() {
 	accountId, _ := strconv.ParseInt(os.Getenv("NEWRELIC_ACCOUNT_ID"), 10, 64)
+	wg := new(sync.WaitGroup)
 
-	// // Unique applications
-	uniqueApps := apps.NewUniqueApps(accountId)
-	uniqueApps.Run()
+	// Unique applications
+	wg.Add(1)
+	go uniqueApps(wg, accountId)
 
 	// Data ingests
+	wg.Add(1)
+	go dataIngests(wg, accountId)
+
+	wg.Wait()
+}
+
+func uniqueApps(
+	wg *sync.WaitGroup,
+	accountId int64,
+) {
+	defer wg.Done()
+	uniqueApps := apps.NewUniqueApps(accountId)
+	uniqueApps.Run()
+}
+
+func dataIngests(
+	wg *sync.WaitGroup,
+	accountId int64,
+) {
+	defer wg.Done()
 	dataIngests := data.NewDataIngests(accountId)
 	dataIngests.Run()
 }
