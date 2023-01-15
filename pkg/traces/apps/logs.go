98e6b44c64099b8c95ed31ce7ab15fa4f6baf71a
uturkarslan <ugurturkarslan@gmail.com>
2023-01-15 21:17:23 +0300

    Clean up

--- pkg/traces/apps/logs.go
+++
@@ -1,7 +0,0 @@
-package apps
-
-const (
-	APPS_UNIQUES_FLUSHING_METRICS            = "flushing metrics"
-	APPS_UNIQUES_GRAPHQL_HAS_RETURNED_ERRORS = "graphql has returned errors"
-	APPS_UNIQUES_LOGS_COULD_NOT_BE_FORWARDED = "logs could not be forwarded"
-)
--- pkg/traces/data/ingest.go
+++ pkg/traces/data/ingest.go
@@ -12,6 +12,12 @@ import (
 	metrics "github.com/utr1903/newrelic-tracker-internal/metrics"
 )
 
+const (
+	APPS_INGESTS_FLUSHING_METRICS            = "flushing metrics"
+	APPS_INGESTS_GRAPHQL_HAS_RETURNED_ERRORS = "graphql has returned errors"
+	APPS_INGESTS_LOGS_COULD_NOT_BE_FORWARDED = "logs could not be forwarded"
+)
+
 const queryTemplate = `
 {
   actor {
