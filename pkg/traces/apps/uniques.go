dd26cb2307039fe7221d4d91e5328fa3b76a2d89
uturkarslan <ugurturkarslan@gmail.com>
2023-01-16 11:01:05 +0300

    Clean up

--- pkg/traces/apps/uniques.go
+++ pkg/traces/apps/uniques.go
@@ -13,8 +13,6 @@ import (
 )
 
 const (
-	APPS_UNIQUES_FLUSHING_METRICS            = "flushing metrics"
-	APPS_UNIQUES_GRAPHQL_HAS_RETURNED_ERRORS = "graphql has returned errors"
 	APPS_UNIQUES_LOGS_COULD_NOT_BE_FORWARDED = "logs could not be forwarded"
 )
 
